9772873bb5dea6d605694aa199944ea7047b7c84
mikaeth95 <159429953+mikaeth95@users.noreply.github.com>
2025-06-20 13:16:21 +0200

    Fix: Escape single quotes in project.jsx for netlify deploy

--- app/components/homepage/projects/project-card.jsx
+++ app/components/homepage/projects/project-card.jsx
@@ -48,16 +48,16 @@ function ProjectCard({ project }) {
           </div>
           <div>
             <span className="ml-4 lg:ml-8 mr-2 text-white">name:</span>
-            <span className="text-gray-400">'</span>
+            <span className="text-gray-400">&apos;</span>
             <span className="text-amber-300">{project.name}</span>
-            <span className="text-gray-400">',</span>
+            <span className="text-gray-400">&apos;,</span>
           </div>
           <div className="ml-4 lg:ml-8 mr-2">
             <span className="text-white">tools:</span>
             <span className="text-gray-400">[</span>
             {project.tools.map((tag, i) => (
               <React.Fragment key={i}>
-                <span className="text-amber-300">'{tag}'</span>
+                <span className="text-amber-300">&apos;{tag}&apos;</span>
                 {i < project.tools.length - 1 && <span className="text-gray-400">, </span>}
               </React.Fragment>
             ))}
@@ -65,12 +65,12 @@ function ProjectCard({ project }) {
           </div>
           <div>
             <span className="ml-4 lg:ml-8 mr-2 text-white">myRole:</span>
-            <span className="text-orange-400">'{project.role}'</span>
+            <span className="text-orange-400">&apos;{project.role}&apos;</span>
             <span className="text-gray-400">,</span>
           </div>
           <div className="ml-4 lg:ml-8 mr-2">
             <span className="text-white">description:</span>
-            <span className="text-cyan-400"> '{project.description}'</span>
+            <span className="text-cyan-400">&apos;{project.description}&apos;</span>
             <span className="text-gray-400">,</span>
           </div>
           <div><span className="text-gray-400">{'};'}</span></div>

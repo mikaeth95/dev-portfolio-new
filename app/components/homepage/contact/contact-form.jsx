7ea2099d7eabc0099bce67c4d946cff35e66f6c2
mikaeth95 <159429953+mikaeth95@users.noreply.github.com>
2025-06-21 20:54:04 +0200

    Update .gitignore and project files

--- app/components/homepage/contact/contact-form.jsx
+++ app/components/homepage/contact/contact-form.jsx
@@ -39,6 +39,7 @@ function ContactForm() {
     const options = { publicKey: process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY };
 
     try {
+      console.log(input)
       const res = await emailjs.send(serviceID, templateID, input, options);
 
       if (res.status === 200) {
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Michael Gonaus",
   profile: '/profile3.png',
   designation: "Junior Software Developer",
-  description: "My name is Michael Gonaus and I'm a junior software developer with a strong passion for technology. I began programming in 2023 where I initially explored various programming languages and concepts with the help of AI tools. This approach allowed me to quickly experiment with different technologies and frameworks. In June 2025, I decided to deepen my understanding of programming fundamentals by enrolling in Harvard's CS50P (Python) course where I am now learning the foundational syntax of my first programming language in depth. I'm always eager to explore new technologies, solve challenging problems and grow as a developer.",
+  description: "My name is Michael Gonaus and I'm a junior software developer with a strong passion for technology. I began programming in 2023 where I initially explored various programming languages and concepts with the help of AI tools. This approach allowed me to quickly experiment with different technologies and frameworks. In May 2025, I decided to deepen my understanding of programming fundamentals by enrolling in Harvard's CS50P (Python) course where I am now learning the foundational syntax of my first programming language in depth. I'm always eager to explore new technologies, solve challenging problems and grow as a developer.",
   email: 'contact@michael-gonaus.dev',
   phone: '+43 664 2138582',
   address: 'Austria',
